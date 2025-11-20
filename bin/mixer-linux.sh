f8d93293e1e8e9ef289f1388851aa3e9bbff09df
mohmafia <61617857+mohmafia@users.noreply.github.com>
2025-11-20 13:09:12 +0100

    Updated Binary

---
+++ bin/mixer-linux.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+echo -ne '\033c\033]0;mixer-project-linux\a'
+base_path="$(dirname "$(realpath "$0")")"
+"$base_path/mixer-linux" "$@"
--- bin/mixer.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-echo -ne '\033c\033]0;mixer-project-linux\a'
-base_path="$(dirname "$(realpath "$0")")"
-"$base_path/mixer.x86_64" "$@"
